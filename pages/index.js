67d146e13a3fcdfbf3ec7d7ddc46d1f0ff955fe3
imronxz <imronxz@gmail.com>
2022-02-15 02:00:37 +0700

    01. Setup Spotify Clone with Tailwind & @heroicons

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import SideBar from '../components/SideBar';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -10,6 +11,11 @@ export default function Home() {
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <h1>This is a DOPE spotify 2.0 build</h1>
+
+      <main>
+        <SideBar/>
+      </main>
+      <div>{/* Player */}</div>
     </div>
   );
 }
